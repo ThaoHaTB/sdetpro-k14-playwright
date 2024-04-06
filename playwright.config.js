208e8f6dc82469800b3743f7e9b9a0e566675962
ThaoHaTB <hathaok37cntt@gmail.com>
2024-04-06 22:17:05 +0700

    day 19

---
+++ playwright.config.js
@@ -0,0 +1,23 @@
+const {defineConfig,devices}= require('@playwright/test')
+
+module.exports= defineConfig({
+    //test Directory
+    testDir: './tests',
+    projects: [
+        {
+            name: 'chromium',
+            use: { ...devices['Desktop Chrome'] },
+            headless: false,
+        },
+    ],
+    reporter: 'html',
+    // CI environment
+    retries: process.env.CI ? 1 : 0,
+    // local
+    use: {
+        actionTimeout: 5*1000, // tìm kiếm tối đa 5 giây, giống implicit wait
+        trace: 'on-first-retry',
+        video: 'on-first-retry',
+        screenshot: 'only-on-failure',
+    },
+});
--- tests/Login.spec.ts
+++ tests/Login.spec.ts
@@ -1,1 +1,76 @@
-import {test,Page} from '@playwright/test';
+import { test } from '@playwright/test';
+
+test('Link text -Xpath', async ({page}) => {
+    await page.goto('https://the-internet.herokuapp.com/');
+    //await page.locator('teocodon').click(); //=> locator-> instance
+    const footerLinkEle=await page.waitForSelector('//a[contains(text(),"Elemental")]',{timeout:10000});
+    footerLinkEle.click();
+
+    // Debug only
+    await page.waitForTimeout(1000);
+});
+
+test('Link text - CSS', async ({page}) => {
+    await page.goto('https://the-internet.herokuapp.com/');
+    //await page.locator('teocodon').click(); //=> locator-> instance
+    const footerLinkEle=await page.locator('a: has-text("Elemental")');
+    footerLinkEle.click();
+
+    // Debug only
+    await page.waitForTimeout(1000);
+});
+
+test('Link text - Filter', async ({page}) => {
+    await page.goto('https://the-internet.herokuapp.com/');
+    //await page.locator('teocodon').click(); //=> locator-> instance
+    const footerLinkEle=await page.locator('a').filter({hasText:'Elemental'});
+    footerLinkEle.click();
+
+    // Debug only
+    await page.waitForTimeout(1000);
+});
+
+test('Link text - MultipleMtaching', async ({page}) => {
+    await page.goto('https://the-internet.herokuapp.com/');
+    //await page.locator('teocodon').click(); //=> locator-> instance
+    const footerLinkEle=await page.locator('a').elementHandles();
+    await footerLinkEle[10].click();
+
+    // Debug only
+    await page.waitForTimeout(1000);
+});
+
+
+test('Handle Login form', async ({page}) => {
+    await page.goto('https://the-internet.herokuapp.com/');
+    //Navigate
+    await page.locator('a').filter({hasText:"Form Authentication"}).click();
+    await page.waitForLoadState('domcontentloaded');
+
+    //Form interaction
+    await page.locator('#username').fill('tomsmith');
+    await page.locator('#password').fill('SuperSecretPassword!');
+    await page.locator('button[type="submit"]').click();
+    await page.waitForLoadState('domcontentloaded');
+
+    // Debug only
+    await page.waitForTimeout(1000);
+});
+
+test.only('Element text', async ({page}) => {
+    await page.goto('https://the-internet.herokuapp.com/');
+    //Navigate
+    await page.locator('a').filter({hasText:"Form Authentication"}).click();
+    await page.waitForLoadState('domcontentloaded');
+
+    //Form interaction
+    await page.locator('#username').fill('tomsmith');
+    await page.locator('#password').fill('SuperSecretPassword!');
+    await page.locator('button[type="submit"]').click();
+    await page.waitForLoadState('domcontentloaded');
+
+    const textContent= await page.locator('h4').textContent(); //=> get all text kể cả hiden
+    const innerText= await page.locator('h4').innerText(); //=> chỉ lấy text visible trên UI
+    // Debug only
+    await page.waitForTimeout(1000);
+});
